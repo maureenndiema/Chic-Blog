ea3d58b543e4bcc52234e988474075dc33dde0a4
maureenndiema <ndiemam@gmail.com>
2022-05-15 23:13:19 +0300

    input Login components

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import ValidationError, StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[InputRequired()])
+    password = PasswordField('Password',validators=[InputRequired()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
