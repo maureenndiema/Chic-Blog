80de11575f02520df679aa9d2099f91f4446be25
maureenndiema <ndiemam@gmail.com>
2022-05-15 23:22:52 +0300

    email and username validation function

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -15,3 +15,11 @@ class RegForm(FlaskForm):
     password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("The Email is already in use!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("The username is already in use!")
