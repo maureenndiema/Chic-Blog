f81122f78a1b0c27c0216c368d0228e7232de57d
maureenndiema <ndiemam@gmail.com>
2022-05-15 17:57:01 +0300

    create child class and parent class

--- config.py
+++ config.py
@@ -6,13 +6,34 @@ class Config:
     '''
     pass
 
+# email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
 
 class ProdConfig(Config):
-    pass
-
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = ''
+    
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI =''
 
 class DevConfig(Config):
-    DEBUG = True
+       '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+SQLALCHEMY_DATABASE_URI =''
+DEBUG = True
 
 config_options = {
 'development':DevConfig,
