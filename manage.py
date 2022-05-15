706bb6e9c3feec9c9c4335378a76beaf4ae1d0e9
maureenndiema <ndiemam@gmail.com>
2022-05-15 17:23:59 +0300

    activate procfile, start.sh

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@ from flask_script import Manager,Server
 from app.models import User,Blog
 from flask_migrate import Migrate, MigrateCommand
 
-
-
-
 app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 if __name__ == '__main__':
