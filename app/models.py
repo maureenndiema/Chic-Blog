4afb0846f782043ea05669d0315959766e2e55ec
maureenndiema <ndiemam@gmail.com>
2022-05-16 18:51:42 +0300

    create class User function

--- app/models.py
+++ app/models.py
@@ -13,3 +13,15 @@ class Quote:
     def __init__(self,author,quote):
         self.author=author
         self.quote=quote
+
+class User (UserMixin, db.Model):
+    __tablename__ ='users'
+    id= db.Column(db.Integer,primary_key =True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blogs = db.relationship('Blog', backref='user', lazy='dynamic')
