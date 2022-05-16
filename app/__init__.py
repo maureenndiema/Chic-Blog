14b29dea84832efeb395a003b2d791078a18ef91
maureenndiema <ndiemam@gmail.com>
2022-05-16 18:21:19 +0300

    import config_options

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
-# from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 # bootstrap = Bootstrap()
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,36 @@ from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from ..email import mail_message
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(firstname=form.firstname.data,lastname=form.lastname.data,username=form.username.data,email=form.email.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message("Welcome to My Chic Blog","email/welcome_user", user.email, user=user)
+
+        return redirect(url_for("auth.login"))
+    title = "Sign Up to My Chic Blog"
+    return render_template("auth/register.html", registration_form=form,title=title)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid email or Password')
+
+    title = "Chic Blog login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
