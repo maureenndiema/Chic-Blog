ed8bb659ee405042caab6a37618169581f4ac186
maureenndiema <ndiemam@gmail.com>
2022-05-16 18:28:10 +0300

    register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -35,3 +35,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
     simple.init_app(app)
+
+    # Registering the main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
