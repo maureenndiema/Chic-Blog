0a4f827555d27d2c1bdebf986219c563a1a3f873
maureenndiema <ndiemam@gmail.com>
2022-05-16 21:26:46 +0300

    create tests functions for instance

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -13,3 +13,19 @@ class BlogModelTest(unittest.TestCase):
         """
         self.user_Moh = User(username='Mandi',password='1234', email='mandi@gmail.com')
         self.blog = Blog(title='Monday', blog='This is the ghetto', user=self.user_Moh)
+
+    def tearDown(self):
+        Blog.query.delete()
+        User.query.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.blog, Blog))
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.blog.title, 'Monday')
+        self.assertEquals(self.blog.blog, 'This is the ghetto')
+        self.assertEquals(self.blog.user, self.user_Vee)
+
+    def test_save_blog(self):
+        self.blog.save_blog()
+        self.assertTrue(len(Blog.query.all()) > 0)
