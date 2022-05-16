eff3b3b2c32f13c32fc1e0dfea85bfadfd16dfe7
maureenndiema <ndiemam@gmail.com>
2022-05-16 19:22:10 +0300

    create function for url request

--- app/request.py
+++ app/request.py
@@ -4,3 +4,17 @@ import requests
 
 #Getting the quotes base url
 base_url = None
+
+def configure_request(app):
+    global base_url
+    base_url = app.config["QOUTES_API_BASE_URL"]
+
+
+def get_quotes():
+    '''
+    Function that gets the json response to the url request
+    '''
+    response=requests.get(base_url).json()
+    random_quote = Quote(response.get("author"), response.get("quote"))
+
+    return random_quote
