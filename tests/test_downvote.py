cdb9b68ad2ea885cc94ab38d1134d134d9be45f0
maureenndiema <ndiemam@gmail.com>
2022-05-17 00:19:22 +0300

    mport unittests, blog.db

--- tests/test_downvote.py
+++ tests/test_downvote.py
@@ -14,3 +14,19 @@ class DownvoteModelTest(unittest.TestCase):
         self.user_Moh = User(username='Mandi',password='1234', email='mandi@gmail.com')
         self.blog_Moh = Blog(title='love', blog='This is the ghetto', user=self.user_Moh)
         self.downvote_moh = Downvote(downvote='8', blog=self.blog_moh)
+
+    def tearDown(self):
+        Downvote.query.delete()
+        Blog.query.delete()
+        User.query.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.downvote_moh, Downvote))
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.downvote_moh.downvote, '8')
+        self.assertEquals(self.downvote_moh.blog, self.blog_moh)
+
+    def test_save_upvote(self):
+        self.downvote_moh.save_downvote()
+        self.assertTrue(len(Downvote.query.all()) > 0)
---
+++ tests/tet_subscriber.py
@@ -0,0 +1,5 @@
+import unittest
+from app.models import Subscriber
+from app import db
+
+
