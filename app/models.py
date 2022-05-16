14f630f5d55e86654b4bad85f9d86dbdf7650c89
maureenndiema <ndiemam@gmail.com>
2022-05-16 18:49:48 +0300

    create quote function

--- app/models.py
+++ app/models.py
@@ -3,3 +3,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Quote:
+    
+    def __init__(self,author,quote):
+        self.author=author
+        self.quote=quote
