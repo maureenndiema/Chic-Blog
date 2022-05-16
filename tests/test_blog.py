7e2502c7e88f1ec7b5406ceb7e9d3208dcd4c4ae
maureenndiema <ndiemam@gmail.com>
2022-05-16 21:24:45 +0300

    create tests for blog class

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,3 +1,15 @@
 import unittest
 from app.models import Blog, User
 from app import db
+
+class BlogModelTest(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the class
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.user_Moh = User(username='Mandi',password='1234', email='mandi@gmail.com')
+        self.blog = Blog(title='Monday', blog='This is the ghetto', user=self.user_Moh)
