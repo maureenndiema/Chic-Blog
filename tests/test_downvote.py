64029e15c7467b294ae6066ff181ef9067fd1997
maureenndiema <ndiemam@gmail.com>
2022-05-17 00:15:29 +0300

    create downvote tests

--- tests/test_downvote.py
+++ tests/test_downvote.py
@@ -2,3 +2,15 @@ import unittest
 from app.models import Blog, Downvote, User
 from app import db
 
+class DownvoteModelTest(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the class
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.user_Moh = User(username='Mandi',password='1234', email='mandi@gmail.com')
+        self.blog_Moh = Blog(title='love', blog='This is the ghetto', user=self.user_Moh)
+        self.downvote_moh = Downvote(downvote='8', blog=self.blog_moh)
